6051ec28a97e6309032ce52ed084949259de0efe
cxzaqq <roy1228@naver.com>
2024-02-06 23:13:02 +0900

    Optional Positional Parameters

--- dart_tour/main.dart
+++ dart_tour/main.dart
@@ -7,19 +7,25 @@ arrow 사용 가능
 String sayHello1(String name) => "Hello $name nice to meet you";
 */
 
+//positional parameters 순서 중요
 String sayHello1(String name, int age, String country) {
   return "Hello $name, you are $age, and you come from $country";
 }
 
-//named parameters 사용 시 null safety 때문에 기본값 설정해야 함
+//named parameters 사용 시 null safety 때문에 기본값 설정해야 함. 순서 중요 X.
 String sayHello2(
     {String name = "anon", int age = 99, String country = "wakanda"}) {
   return "Hello $name, you are $age, and you come from $country";
 }
 
-//근데 default value가 아닌 유저에게 직접 입력을 받고 싶다면
+//근데 default value가 아닌 유저에게 직접 입력을 받고 싶다면. 순서 중요 X.
 String sayHello3(
     {required String name, required int age, required String country}) {
+  return "Hello $name, you are $age, and you come from $country";
+}
+
+//parameter를 optional positional로 설정하고 싶다면. 순서 중요. 근데 자주 사용 안 함.
+String sayHello4(String name, int age, [String? country = "wakanda"]) {
   return "Hello $name, you are $age, and you come from $country";
 }
 
@@ -29,4 +35,6 @@ void main() {
   //대신 이렇게 순서는 상관 없음
   print(sayHello2(age: 10, country: "Kor"));
   print(sayHello3(age: 10, country: "Kor", name: "yoon"));
+  //optinal positional 순서 중요.
+  print(sayHello4("yoon", 12));
 }
