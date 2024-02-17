7a13abdb81ccb1a41e583f4406c8ccdaf8c1cff6
cxzaqq <roy1228@naver.com>
2024-02-17 09:53:25 +0900

    ex of using api-data

--- dart_tour/main.dart
+++ dart_tour/main.dart
@@ -1,18 +1,18 @@
 class Player {
   //final 사용 시 변경 불가
   final String name;
-  int xp, age;
+  int xp;
   String team;
 
   //constructor method의 이름은 class의 이름과 같아야 함. arguments 위치 중요, 위에 타입을 정했기 때문에 따로 지정 필요 없음
   //Player(this.name, this.xp, this.team, this.age);
 
   //named constructor로 변경
-  Player(
-      {required this.name,
-      required this.xp,
-      required this.team,
-      required this.age});
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
 
   //class method 내에서 this는 사용하지 않는 것이 권고됨
   void sayHello() {
@@ -24,16 +24,19 @@ class Player {
   Player.createBluePlyaer({
     required String name,
     required int age,
-  })  : this.age = age,
-        this.name = name,
+  })  : this.name = name,
         this.team = 'blue',
         this.xp = 0;
   //positional constructor. 이 경우 parameters는 무조건 required임
   Player.createRedPlayer(String name, int age)
-      : this.age = age,
-        this.name = name,
+      : this.name = name,
         this.team = 'red',
         this.xp = 0;
+
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
 }
 
 /* 위 constructor가 축약형
@@ -50,17 +53,26 @@ class Player {
 */
 
 void main() {
-  //positional
-  //var player = Player("yoon", 1500, 'red', 12);
-  //named
-  var player = Player(
-    name: "yoon",
-    xp: 1500,
-    age: 12,
-    team: "blue",
-  );
-  player.sayHello();
+  var apiData = [
+    {
+      "name": "yoon",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "nico",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "red",
+      "xp": 0,
+    }
+  ];
 
-  var bluePlayer = Player.createBluePlyaer(name: 'blue', age: 12);
-  var redPlayer = Player.createRedPlayer('red', 12);
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
